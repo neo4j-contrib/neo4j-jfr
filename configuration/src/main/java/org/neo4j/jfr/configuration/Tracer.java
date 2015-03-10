976a903b576a9947e7fff5df6337caad0a38f8ae
Lasse Westh-Nielsen <lassewesth@gmail.com>
2015-03-10 23:30:31 +0100

    cli module uses airline to capture commands

--- configuration/src/main/java/org/neo4j/jfr/configuration/Tracer.java
+++ configuration/src/main/java/org/neo4j/jfr/configuration/Tracer.java
@@ -20,10 +20,13 @@
 package org.neo4j.jfr.configuration;
 
 import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 @Target(TYPE)
+@Retention(RUNTIME)
 public @interface Tracer
 {
     String value();
