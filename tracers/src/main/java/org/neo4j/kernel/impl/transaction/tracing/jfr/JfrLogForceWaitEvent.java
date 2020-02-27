febcc0701b9c2e3e391a01e7688362bd02015ae1
Chris Vest <mr.chrisvest@gmail.com>
2020-02-27 14:24:52 +0100

    New year, new licenses.

--- tracers/src/main/java/org/neo4j/kernel/impl/transaction/tracing/jfr/JfrLogForceWaitEvent.java
+++ tracers/src/main/java/org/neo4j/kernel/impl/transaction/tracing/jfr/JfrLogForceWaitEvent.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2002-2019 "Neo Technology,"
+ * Copyright (c) 2002-2020 "Neo Technology,"
  * Network Engine for Objects in Lund AB [http://neotechnology.com]
  *
  * This file is part of Neo4j.
